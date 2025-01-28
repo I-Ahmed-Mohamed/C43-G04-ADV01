85bf2d55059cde58df3b0080ac818b2ca4dcc90c
ahmedmohamedcod <ahmedcodteng1@gmail.com>
2025-01-28 15:59:41 +0200

    Q5

---
+++ Assignment/FirstNonRepeatedCharacter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class FirstNonRepeatedCharacter
+    {
+        public static int FindFirstNonRepeatedCharacterIndex(string input)
+        {
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            // Count the occurrence of each character
+            foreach (char c in input)
+            {
+                if (charCount.ContainsKey(c))
+                {
+                    charCount[c]++;
+                }
+                else
+                {
+                    charCount[c] = 1;
+                }
+            }
+
+            // Find the first character with a count of 1
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (charCount[input[i]] == 1)
+                {
+                    return i;
+                }
+            }
+
+            return -1; // Return -1 if no non-repeated character is found
+        }
+    }
+}
