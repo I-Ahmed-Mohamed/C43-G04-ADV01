51f4c05fa706025b2bce7ef6abf3f2e377c062e2
ahmedmohamedcod <ahmedcodteng1@gmail.com>
2025-01-28 15:49:55 +0200

    Q2

---
+++ Assignment/ArrayListReverser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+   
+
+    public class ArrayListReverser
+    {
+        public static void ReverseInPlace(ArrayList list)
+        {
+            int start = 0;
+            int end = list.Count - 1;
+
+            while (start < end)
+            {
+                object temp = list[start];
+                list[start] = list[end];
+                list[end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
