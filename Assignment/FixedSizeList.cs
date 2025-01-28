15da0e0c45df4c45702448a8343e2d7c24050de1
ahmedmohamedcod <ahmedcodteng1@gmail.com>
2025-01-28 15:52:05 +0200

    Q4

---
+++ Assignment/FixedSizeList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+
+    public class FixedSizeList<T>
+    {
+        private T[] _items;
+        private int _count;
+
+        public FixedSizeList(int capacity)
+        {
+            if (capacity <= 0)
+            {
+                throw new ArgumentException("Capacity must be greater than zero.");
+            }
+            _items = new T[capacity];
+            _count = 0;
+        }
+
+        public void Add(T item)
+        {
+            if (_count >= _items.Length)
+            {
+                throw new InvalidOperationException("The list is full.");
+            }
+            _items[_count] = item;
+            _count++;
+        }
+
+        public T Get(int index)
+        {
+            if (index < 0 || index >= _count)
+            {
+                throw new IndexOutOfRangeException("Index is out of range.");
+            }
+            return _items[index];
+        }
+    }
+}
