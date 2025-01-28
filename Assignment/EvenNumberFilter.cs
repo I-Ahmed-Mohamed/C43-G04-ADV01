ddcf604737d97c402cc5ca63406cb503d0aa4315
ahmedmohamedcod <ahmedcodteng1@gmail.com>
2025-01-28 15:51:00 +0200

    Q3

---
+++ Assignment/EvenNumberFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+  
+
+    public class EvenNumberFilter
+    {
+        public static List<int> FilterEvenNumbers(List<int> numbers)
+        {
+            List<int> evenNumbers = new List<int>();
+
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    evenNumbers.Add(number);
+                }
+            }
+
+            return evenNumbers;
+        }
+    }
+}
